c0d344c4721c549bd0d648eb009a8372d653aff9
Oni Emmanuel <44571157+emmaxyz90@users.noreply.github.com>
2022-09-08 15:56:25 +0100

    Update and rename docker-build.sh to Number 2/docker-build.sh

--- Number 2/docker-build.sh
+++ Number 2/docker-build.sh
@@ -1,19 +1,7 @@
 #!/bin/bash
 
-# dialog  --title "Docker Build Script" --yesno "This script will create Wordpress and MYSQL containers. Make sure you are running this script in docker host. Would you like to execute this script?" 8 60
-
-# response=$?
-
-# if [ $response -eq 1 ]; then
-#   clear >$(tty)
-#   exit 1
-# fi
-
-
 db_vol=$(dialog --title "Docker Build Script" --inputbox "Database Container Volume Name:" 10 40 3>&1 1>&2 2>&3 3>&-) 
 
-#echo $wp_vol $db_vol
-# dialog --title "Docker Build Script" --mixedgauge "Creating Docker Compose File..." 0 0 0 
   if [ ! -e ./docker-compose.yml ]; then
     touch docker-compose.yml
   else
@@ -31,7 +19,7 @@ db_vol=$(dialog --title "Docker Build Script" --inputbox "Database Container Vol
   echo "      - "443:443"" >> docker-compose.yml
   echo "    networks:" >> docker-compose.yml
   echo "      - my_net" >> docker-compose.yml
-# dialog  --title "Docker Build Script" --mixedgauge "Creating Docker Compose File..." 0 0 33
+
   echo "  mysql:" >> docker-compose.yml
   echo "    image: mysql:5.7" >> docker-compose.yml
   echo "    container_name: database" >> docker-compose.yml
@@ -46,7 +34,7 @@ db_vol=$(dialog --title "Docker Build Script" --inputbox "Database Container Vol
   echo "      - MYSQL_PASSWORD=wordpress" >> docker-compose.yml
   echo "    networks:" >> docker-compose.yml
   echo "      - my_net" >> docker-compose.yml
-  # dialog  --title "Docker Build Script" --mixedgauge "Creating Docker Compose File..." 0 0 66
+
   echo "  kibana:" >> docker-compose.yml
   echo "    image: docker.elastic.co/kibana/kibana:6.4.2" >> docker-compose.yml
   echo "    container_name: Kibana" >> docker-compose.yml
@@ -65,15 +53,6 @@ db_vol=$(dialog --title "Docker Build Script" --inputbox "Database Container Vol
   echo "  my_net:" >> docker-compose.yml
 
 
-# dialog --title "Docker Build Script" --mixedgauge "Creating Docker Compose File..." 0 0 100
-
-# dialog  --title "Docker Build Script" --infobox "Please wait while containers are getting created..." 7 40 
-
 docker-compose -f docker-compose.yml up -d
 
-# response=$?
-# if [ $response -eq 0 ]; then 
-# #   dialog  --title "Docker Build Script" --msgbox "Successfully created Wordpress and MYSQL containers." 7 40 
-# # fi
-# clear >$(tty)
 exit 0 
